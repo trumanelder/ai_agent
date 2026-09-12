93893f2ee83c268865513f3fc75fb20a55d87b10
Truman <truman.t.elder@gmail.com>
2026-09-12 10:39:36 -0400

    feat: connected openrouter and added token tracking

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        }
+    ],
+)
+
 def main():
-    print("Hello from ai-agent!")
+    if response.usage == None:
+        raise RuntimeError
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
