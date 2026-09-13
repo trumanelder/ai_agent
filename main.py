56514fa71836f3e0529974a2a39b949d1bca3db0
Truman <truman.t.elder@gmail.com>
2026-09-13 16:59:50 -0400

    feat: add foundating for multiple message conversation and storage. add toggle for metadata output

--- main.py
+++ main.py
@@ -13,23 +13,23 @@ client = OpenAI(
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
+messages = [{"role": "user", "content": args.user_prompt}]
+           
 response = client.chat.completions.create(
     model="openrouter/free",
-    messages=[
-        {
-            "role": "user",
-                "content": args.user_prompt
-        }
-    ],
+    messages=messages,
 )
 
 def main():
     if response.usage == None:
         raise RuntimeError
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
     print(response.choices[0].message.content)
 
 
